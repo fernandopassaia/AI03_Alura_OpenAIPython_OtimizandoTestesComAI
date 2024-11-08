1222f17b71e678ea1e081ee49eacb7f0b293692e
Fernando Passaia <fernandopassaia@futuradata.com.br>
2024-11-08 22:23:52 +0000

    Gerando casos de teste

--- 01_DesenhandoTestesCasosUsoIAGenerativa/main.py
+++ 01_DesenhandoTestesCasosUsoIAGenerativa/main.py
@@ -1,9 +1,13 @@
 from gerador_casos_de_uso import *
+from gerador_cenarios_teste import *
 
 
 def main():
     casos_uso = gerar_caso_uso()
-    print(casos_uso)
+    print("\nCaso de Uso:\n", casos_uso)
+
+    cenario_teste = gerar_cenario_teste(caso_uso=casos_uso)
+    print("\nCenario Teste\n", cenario_teste)
 
 
 if __name__ == "__main__":
